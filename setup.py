6e6426e54296ee88d371e56f0058c27f64f85c0a
LucaBiasia <72028757+LucaBiasia@users.noreply.github.com>
2023-09-22 15:45:13 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="madx-piE5-beamline",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Luca Biasia",
+    author_email="luca.biasia@hotmail.com",
+    description="",
+    long_description="Read the full documentation on GitHub.",
+    license="",
+    url="https://github.com/LucaBiasia/madx-piE5-beamline",
+)
